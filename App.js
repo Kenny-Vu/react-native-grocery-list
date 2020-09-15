f2081fd6dc50f97b8afb2dbada44a7bbf5c9c4e6
Kenny Vu <kennyvu@live.ca>
2020-09-15 17:41:47 -0400

    first commit. Basic hard coded shopping list done

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { v4 as uuidv4 } from "uuid";
 
-export default function App() {
+import Header from "./components/Header";
+import ListItem from "./components/ListItem";
+
+const App = () => {
+  const [items, setItems] = useState([
+    { id: uuidv4(), text: "Milk" },
+    { id: uuidv4(), text: "Bread" },
+    { id: uuidv4(), text: "Sugar" },
+    { id: uuidv4(), text: "Coffee" },
+  ]);
+
+  // const removeItem = (id) => {
+  //   setItems((prev) => {
+  //     return prev.filter((item) => item.id !== id);
+  //   });
+  // };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <ListItem item={item} />}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
   },
 });
+
+export default App;
